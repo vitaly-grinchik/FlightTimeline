081d23e96a7ff49f9fab644da330688a36bacbad
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-08-10 18:43:06 +0300

    Add files

---
+++ FlightTimeline/FlightDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  FlightDetailsView.swift
+//  FlightTimeline
+//
+//  Created by Виталий Гринчик on 10.08.23.
+//
+
+import SwiftUI
+
+struct FlightDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlightDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightDetailsView()
+    }
+}
---
+++ FlightTimeline/FlightRowView.swift
@@ -0,0 +1,20 @@
+//
+//  FlightRowView.swift
+//  FlightTimeline
+//
+//  Created by Виталий Гринчик on 10.08.23.
+//
+
+import SwiftUI
+
+struct FlightRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlightRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightRowView()
+    }
+}
